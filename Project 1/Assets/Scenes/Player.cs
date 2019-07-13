19c467b9d4d5864880e3f25c2576950fc31ce69f
PatLiu1168 <39655877+PatLiu1168@users.noreply.github.com>
2019-07-13 18:40:59 -0400

    new project

---
+++ Project 1/Assets/Scenes/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//1f is like an undefined number
+
+public class Player : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Vectfinish;
+    // Start is called before the first frame update
+    void Start() 
+    {
+        print("I am a cube"); 
+        moveSpeed =5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime == finish){
+            print ("done");
+        }
+        transform.Translate (moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
